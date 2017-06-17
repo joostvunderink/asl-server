ec5db17a5345176c74ba9879ba7377cab8164b14
Joost Vunderink <joost@vunderink.net>
2017-06-17 22:08:18 +0200

    Refactored the routers.

---
+++ src/core/base.router.ts
@@ -0,0 +1,111 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+export default class BaseRouter {
+  router: Router;
+
+  constructor(public controller) {
+    this.router = Router();
+    this.init();
+  }
+
+  getAll(req: Request, res: Response, next: NextFunction) {
+    this.controller.getAll()
+    .then((ret) => {
+      res.status(200).send(ret);
+    });
+  }
+
+  public getOne(req: Request, res: Response, next: NextFunction) {
+    this.controller.getOne(req.params.id)
+    .then(ret => {
+      res.status(200).send(ret);
+     })
+    .catch(err => {
+      if (err.name === 'CustomError' && err.message === 'EmptyResponse') {
+        res.status(404).send();
+      }
+      else if (err.name.endsWith('NotFoundError')) {
+        res.status(404).send();
+      }
+      else {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  public create(req: Request, res: Response, next: NextFunction) {
+    this.controller.create(req.body)
+    .then((createdEntity) => {
+      // TODO: Add Location header. Figure out how to determine the base API URL here.
+      res.status(201).send(createdEntity);
+    })
+    .catch(err => {
+      if (err.code && err.code.startsWith('ER_')) {
+        res.status(400).send({
+          code: err.code,
+          message: err.message,
+        });
+      }
+      else {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  public update(req: Request, res: Response, next: NextFunction) {
+    this.controller.update(req.params.id, req.body)
+    .then((updatedEntity) => {
+      res.status(200).send(updatedEntity);
+    })
+    .catch(err => {
+      if (err.name === 'CustomError' && err.message === 'EmptyResponse') {
+        res.status(404).send();
+      }
+      else if (err.name.endsWith('NotFoundError')) {
+        res.status(404).send();
+      }
+      else {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  public deleteOne(req: Request, res: Response, next: NextFunction) {
+    this.controller.delete(req.params.id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(err => {
+      if (err.name === 'CustomError' && err.message === 'EmptyResponse') {
+        res.status(404).send();
+      }
+      else if (err.name.endsWith('NotFoundError')) {
+        res.status(404).send();
+      }
+      else {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  init() {
+    let self = this;
+
+    function wrap(fname) {
+      // This wrap trick is needed to make sure the methods are called from
+      // the context of the instance, instead of 'bare'. This way, we make
+      // sure that 'this' is available in the methods.
+      // After all, express has no idea that the router function is actually
+      // an instance method.
+      return function(req: Request, res: Response, next: NextFunction) {
+        return self[fname](req, res, next);
+      }
+    }
+
+    this.router.get('/',       wrap('getAll'));
+    this.router.get('/:id',    wrap('getOne'));
+    this.router.post('/',      wrap('create'));
+    this.router.put('/:id',    wrap('update'));
+    this.router.delete('/:id', wrap('deleteOne'));
+  }
+}
