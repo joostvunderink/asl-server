19f210bba74d5b9400e0c9cc549c791efdced878
Joost Vunderink <joost@vunderink.net>
2017-06-21 20:09:30 +0200

    Removed old and useless imports

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,6 @@ import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 import getRouteConfig from './routes';
 
-import ClubRouter                from './api/club/club.router';
-import CompetitionRouter         from './api/competition/competition.router';
-import CompetitionTemplateRouter from './api/competition-template/competition-template.router';
-import CountryRouter             from './api/country/country.router';
-import PersonRouter              from './api/person/person.router';
-import RegionRouter              from './api/region/region.router';
-import SeasonRouter              from './api/season/season.router';
-import SportRouter               from './api/sport/sport.router';
-import TeamRouter                from './api/team/team.router';
-
-
 // Creates and configures an ExpressJS web server.
 class App {
 
