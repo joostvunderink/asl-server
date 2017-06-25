46873b8ab86723b7be7c9649a4df6e3436cefa7a
Joost Vunderink <joost@vunderink.net>
2017-06-25 16:06:31 +0200

    Added test for where/like.

--- test/filter.where.test.ts
+++ test/filter.where.test.ts
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('Filter: where, operator: equals', () => {
-  it('finds 3 regions where country_id = 1', () => {
+  it('finds regions where country_id = 1', () => {
     return chai.request(app).get('/regions?filter={"where":{"country_id":1}}')
       .then(res => {
         expect(res.status).to.equal(200);
@@ -20,7 +20,7 @@ describe('Filter: where, operator: equals', () => {
         });
       });
   });
-  it('finds 2 regions where country_id = 1 and sport_id = 1', () => {
+  it('finds regions where country_id = 1 and sport_id = 1', () => {
     return chai.request(app).get('/regions?filter={"where":{"country_id":1,"sport_id":1}}')
       .then(res => {
         expect(res.status).to.equal(200);
@@ -33,15 +33,16 @@ describe('Filter: where, operator: equals', () => {
         });
       });
   });
-  it('finds 1 region where sport_id = 2', () => {
-    return chai.request(app).get('/regions?filter={"where":{"sport_id":2}}')
+  it('finds regions where sport_id = 1 and region name starts with "West"', () => {
+    return chai.request(app).get('/regions?filter={"where":{"sport_id":1,"name":{"like":"West%25"}}}')
       .then(res => {
         expect(res.status).to.equal(200);
         expect(res).to.be.json;
         expect(res.body).to.be.an('array');
-        expect(res.body).to.have.length(1);
+        expect(res.body).to.have.length(2);
         res.body.forEach(region => {
-          expect(region.sport_id).to.equal(2);
+          expect(region.sport_id).to.equal(1);
+          expect(region.name).to.match(/^West/);
         });
       });
   });
