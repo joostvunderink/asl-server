fcfbdbcdaba6946b4313d6ce25d699e5c3994eb8
Joost Vunderink <joost@vunderink.net>
2017-07-11 08:23:21 +0200

    Fixed bug in config

--- src/config/config.development.ts
+++ src/config/config.development.ts
@@ -12,6 +12,6 @@ let config = {
     },
   }
 };
-config = {"db":{"client":"mysql","connection":{"host":"eu-cdbr-west-01.cleardb.com","port":3306,"user":"b573f341986a5b","password":"6c99c34d","database":"heroku_8367dca6932f169","charset":"utf8"}}};
-config = {"db":{"client":"mysql","connection":{"host":"wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","port":3306,"user":"mxogfzzmjdc51ds1","password":"peoiugbpo041ncq0","database":"di1wglxm9ngblkzy","charset":"utf8"}}};
+// config = {"db":{"client":"mysql","connection":{"host":"eu-cdbr-west-01.cleardb.com","port":3306,"user":"b573f341986a5b","password":"6c99c34d","database":"heroku_8367dca6932f169","charset":"utf8"}}};
+// config = {"db":{"client":"mysql","connection":{"host":"wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","port":3306,"user":"mxogfzzmjdc51ds1","password":"peoiugbpo041ncq0","database":"di1wglxm9ngblkzy","charset":"utf8"}}};
 export default config;
