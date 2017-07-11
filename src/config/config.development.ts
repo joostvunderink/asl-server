0a915ddf1c0b2ebb57a8f86ab062fe4fb424bbfe
Joost Vunderink <joost@vunderink.net>
2017-07-11 08:36:57 +0200

    Knex debug now linked to ASL_DB_DEBUG env var

--- src/config/config.development.ts
+++ src/config/config.development.ts
@@ -1,8 +1,8 @@
 let config = {
   db: {
     client: 'mysql',
-    debug: process.env.ASL_DB_DEBUG,
     connection: {
+      debug    : process.env.ASL_DB_DEBUG,
       host     : process.env.ASL_DB_HOST || 'asl-db',
       port     : process.env.ASL_DB_PORT || 3306,
       user     : process.env.ASL_DB_USER || 'asl',
@@ -12,6 +12,5 @@ let config = {
     },
   }
 };
-// config = {"db":{"client":"mysql","connection":{"host":"eu-cdbr-west-01.cleardb.com","port":3306,"user":"b573f341986a5b","password":"6c99c34d","database":"heroku_8367dca6932f169","charset":"utf8"}}};
-// config = {"db":{"client":"mysql","connection":{"host":"wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","port":3306,"user":"mxogfzzmjdc51ds1","password":"peoiugbpo041ncq0","database":"di1wglxm9ngblkzy","charset":"utf8"}}};
+// config = {"db":{"client":"mysql","connection":{"debug": config.db.connection.debug,"host":"wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","port":3306,"user":"mxogfzzmjdc51ds1","password":"peoiugbpo041ncq0","database":"di1wglxm9ngblkzy","charset":"utf8"}}};
 export default config;
