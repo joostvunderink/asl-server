c3949ca324ddf16512f5bb81b285f29e03fa4d0b
Joost Vunderink <joost@vunderink.net>
2017-07-11 13:28:33 +0200

    Knex now uses new config structure.

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,13 @@
-const config = require('./dist/config');
-const knexConf = config.default.db;
+const { config } = require('./dist/config');
+const knexConf = config.db;
 
-const unittestConfig = require('./dist/config/config.unittest');
-const unittestKnexConf = unittestConfig.default.db;
+const { unittestConfig } = require('./dist/config/config.unittest');
+const unittestKnexConf = unittestConfig.db;
 
 module.exports = {
   development: knexConf,
-  staging    : knexConf,
+  test       : knexConf,
   qa         : knexConf,
-  release    : knexConf,
-  demo       : knexConf,
   production : knexConf,
-  prod       : knexConf,
   unittest   : unittestKnexConf
 };
