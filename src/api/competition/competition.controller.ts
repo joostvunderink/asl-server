3d225a96459ba6152141ff248d0d95062154fb45
Joost Vunderink <joost@vunderink.net>
2017-08-27 10:17:11 +0200

    Rank is now also calculated for competitions.

--- src/api/competition/competition.controller.ts
+++ src/api/competition/competition.controller.ts
@@ -21,48 +21,92 @@ export class CompetitionController extends BaseController {
       const rounds = competition.related('rounds').toJSON();
       const matches = competition.related('matches').toJSON();
 
-      // logger.info({ teams }, 'the teams');
-
-      let matchResults = {};
+      
+      let teamsResults = {};
       teams.forEach(team => {
-        matchResults[team.id] = {
+        teamsResults[team.id] = {
           num_matches_played: 0,
           num_matches_win   : 0,
           num_matches_loss  : 0,
           num_matches_draw  : 0,
           goals_scored      : 0,
           goals_received    : 0,
+          rank              : 1,
+          points            : 0,
+          rankScore         : 0,
         };
       });
 
+      let maxMatchesPlayed = 0;
       matches.forEach(match => {
         // logger.info({ match }, 'le match');
         if (match.home_team_score > match.away_team_score) {
-          matchResults[match.home_team_id].num_matches_win += 1;
-          matchResults[match.away_team_id].num_matches_loss += 1;
+          teamsResults[match.home_team_id].num_matches_win += 1;
+          teamsResults[match.away_team_id].num_matches_loss += 1;
+          teamsResults[match.home_team_id].points += 3;
         } else if (match.home_team_score < match.away_team_score) {
-          matchResults[match.home_team_id].num_matches_loss += 1;
-          matchResults[match.away_team_id].num_matches_win += 1;
+          teamsResults[match.home_team_id].num_matches_loss += 1;
+          teamsResults[match.away_team_id].num_matches_win += 1;
+          teamsResults[match.away_team_id].points += 3;
         } else {
-          matchResults[match.home_team_id].num_matches_draw += 1;
-          matchResults[match.away_team_id].num_matches_draw += 1;
+          teamsResults[match.home_team_id].num_matches_draw += 1;
+          teamsResults[match.away_team_id].num_matches_draw += 1;
+          teamsResults[match.home_team_id].points += 1;
+          teamsResults[match.away_team_id].points += 1;
         }
         
-        matchResults[match.home_team_id].num_matches_played += 1;
-        matchResults[match.home_team_id].goals_scored += match.home_team_score;
-        matchResults[match.home_team_id].goals_received += match.away_team_score;
+        teamsResults[match.home_team_id].num_matches_played += 1;
+        teamsResults[match.home_team_id].goals_scored += match.home_team_score;
+        teamsResults[match.home_team_id].goals_received += match.away_team_score;
 
-        matchResults[match.away_team_id].num_matches_played += 1;
-        matchResults[match.away_team_id].goals_scored += match.away_team_score;
-        matchResults[match.away_team_id].goals_received += match.home_team_score;
+        teamsResults[match.away_team_id].num_matches_played += 1;
+        teamsResults[match.away_team_id].goals_scored += match.away_team_score;
+        teamsResults[match.away_team_id].goals_received += match.home_team_score;
 
-        const away_team_win = match.home_team_score > match.away_team_score;
+        // We need the maximum amount of matches played for a single team for
+        // the rank calculation later on.
+        if (teamsResults[match.home_team_id].num_matches_played > maxMatchesPlayed) {
+          maxMatchesPlayed = teamsResults[match.home_team_id].num_matches_played;
+        }
+        if (teamsResults[match.away_team_id].num_matches_played > maxMatchesPlayed) {
+          maxMatchesPlayed = teamsResults[match.away_team_id].num_matches_played;
+        }
       });
 
-      // logger.info({ matchResults }, 'results');
+      // Determine a special rank score for each team's results.
+      // This score will allow a simple numerical sort to determine the teams'
+      // rankings.
+      // The score contains the following numbers:
+      // 1. Verliespunten: 900 - (3 * max_num_matches_played) + points
+      // 2. Points
+      // 3. Doelsaldo: goals_scored - goals_received
+      // 4. Goals: goals_scored
 
-      let promises = Object.keys(matchResults).map(teamId => {
-        const teamResults = matchResults[teamId];
+      // teamRanks is for later use, to sort the rank scores to order the teams.
+      let teamRanks = [];
+      teams.forEach(team => {
+        let tr = teamsResults[team.id];
+        const verliespunten = 
+        tr.rankScore = 
+          1000000000 * (999 - (3 * maxMatchesPlayed) + tr.points) +
+             1000000 * tr.points +
+                1000 * (tr.goals_scored - tr.goals_received) +
+                   1 * tr.goals_scored;
+        teamRanks.push({ team_id: team.id, rankScore: tr.rankScore });
+      });
+   
+      // Order the teams by the rank score.
+      teamRanks = teamRanks.sort((a, b) => {
+        return b.rankScore - a.rankScore;
+      });
+
+      // Update the team results with the ranks.
+      teamRanks.forEach((tr, index) => {
+        teamsResults[tr.team_id].rank = index + 1;
+      });
+
+      let promises = Object.keys(teamsResults).map(teamId => {
+        const teamResults = teamsResults[teamId];
         return CompetitionTeam.where('id', teamId).fetch()
         .then(competitionTeam => {
           // logger.info({ team_id: teamId, num_matches_played: teamResults.num_matches_played },
@@ -74,6 +118,8 @@ export class CompetitionController extends BaseController {
             'num_matches_draw',
             'goals_scored',
             'goals_received',
+            'points',
+            'rank',
           ];
           fields.forEach(field => {
             competitionTeam.set(field, teamResults[field]);
--- test/competition.calculate.test.ts
+++ test/competition.calculate.test.ts
@@ -12,7 +12,7 @@ import competitionController from '../src/api/competition/competition.controller
 const expect = chai.expect;
 
 describe('Competition calculate standings', () => {
-  it('is a function', () => {
+  it('calculates the standings', () => {
     const competitionId = 2;
     return competitionController.calculateStandings({ competitionId: competitionId })
     .then(() => {
@@ -24,22 +24,23 @@ describe('Competition calculate standings', () => {
 
       const expectedStandings = [
         // team_id, num_matches_played, num_matches_win, num_matches_loss, num_matches_draw,
-        // goals_scored, goals_received
-        //TI  MP  MW  ML  MD  GS  GR
-        [  1,  2,  0,  2,  0,  4,  6, ],
-        [  2,  2,  1,  0,  1,  5,  4, ],
-        [  3,  2,  2,  0,  0,  9,  5, ],
-        [  4,  2,  1,  1,  0,  8,  5, ],
-        [  5,  2,  1,  1,  0,  3,  6, ],
-        [  6,  2,  2,  0,  0, 10,  1, ],
-        [  7,  2,  0,  2,  0,  2,  6, ],
-        [  8,  2,  1,  0,  1,  7,  3, ],
-        [  9,  2,  0,  0,  2,  4,  4, ],
-        [ 10,  2,  0,  2,  0,  5, 11, ],
-        [ 11,  2,  1,  1,  0,  2,  3, ],
-        [ 12,  2,  1,  1,  0,  3,  2, ],
-        [ 13,  2,  1,  1,  0,  5, 11, ],
-        [ 14,  2,  0,  0,  2,  3,  3, ],
+        // goals_scored, goals_received,
+        // points, rank
+        //TI  MP  MW  ML  MD  GS  GR  PT  RK
+        [  1,  2,  0,  2,  0,  4,  6,  0, 12],
+        [  2,  2,  1,  0,  1,  5,  4,  4,  4],
+        [  3,  2,  2,  0,  0,  9,  5,  6,  2],
+        [  4,  2,  1,  1,  0,  8,  5,  3,  5],
+        [  5,  2,  1,  1,  0,  3,  6,  3,  8],
+        [  6,  2,  2,  0,  0, 10,  1,  6,  1],
+        [  7,  2,  0,  2,  0,  2,  6,  0, 13],
+        [  8,  2,  1,  0,  1,  7,  3,  4,  3],
+        [  9,  2,  0,  0,  2,  4,  4,  2, 10],
+        [ 10,  2,  0,  2,  0,  5, 11,  0, 14],
+        [ 11,  2,  1,  1,  0,  2,  3,  3,  7],
+        [ 12,  2,  1,  1,  0,  3,  2,  3,  6],
+        [ 13,  2,  1,  1,  0,  5, 11,  3,  9],
+        [ 14,  2,  0,  0,  2,  3,  3,  2, 11],
       ];
       const teams = competition.related('teams').toJSON();
 
@@ -52,6 +53,8 @@ describe('Competition calculate standings', () => {
         expect(team.num_matches_draw  ).to.equal(es[4]);
         expect(team.goals_scored      ).to.equal(es[5]);
         expect(team.goals_received    ).to.equal(es[6]);
+        expect(team.points            ).to.equal(es[7]);
+        expect(team.rank              ).to.equal(es[8]);
       });
     });
   });
